45d5c2926883fad5ed5db41bdb4315c6c4f69a57
Peter Lawson <lawsonpd@pm.me>
2022-02-14 20:25:51 -0600

    Use Decimal for adding floats. Generalize chaosgame to generate n-sided polygon fractals.

--- chaosgame.py
+++ chaosgame.py
@@ -2,9 +2,17 @@ import tkinter as tk
 from tkinter import Tk
 import math
 import random
+import itertools
+import decimal
+from decimal import Decimal
+
+decimal.getcontext().prec = 5
+
+def round_dec(n):
+    return float(Decimal(n))
 
 def euc_dist(point1:tuple, point2:tuple) -> tuple:
-    return math.sqrt((point2[1] - point1[1])**2 + (point2[0] - point1[0])**2)
+    return round_dec(math.sqrt((point2[1] - point1[1])**2 + (point2[0] - point1[0])**2))
 
 def midpoint(point1:tuple, point2:tuple) -> tuple:
     return ((point1[0] + point2[0])/2, (point1[1] + point2[1])/2)
@@ -16,55 +24,52 @@ def inter_point(point1:tuple, point2:tuple, jump_factor:float) -> tuple:
     '''
     return (int((point1[0] + point2[0]) * jump_factor), int((point1[1] + point2[1]) * jump_factor))
 
-def polygon_height(polygon:tuple) -> float:
+def gamma(a:float, b:float, c:float):
     '''
-    Return "height" (distance from top vertex to baseline).
-    NOTE: Assumes polygon has one side that's parallel to the x-axis.
+    Returns angle given three points using law of cosines.
     '''
-    return max(polygon, key=lambda p: p[1])[1] - min(polygon, key=lambda p: p[1])[1]
+    return round_dec(math.degrees(math.acos( (a**2 + b**2 - c**2) / (2 * a * b) )))
 
-def special_dist(polygon:tuple, point:tuple) -> bool:
+def is_contained(polygon:tuple, test_point:tuple) -> bool:
     '''
-    Determines whether a point is less than D distance from
-    all vertices in polygon, where D is shortest distance
-    across the polygon.
+    
     '''
-    d = polygon_height(polygon)
-    for vertex in polygon:
-        if euc_dist(point, vertex) > d:
-            return False
-    return True
+    dists_to_point = [euc_dist(vertex, test_point) for vertex in polygon]
+    # edge_length = euc_dist(polygon[0], polygon[1])
+    edge_lengths = [euc_dist(v1, v2) for v1, v2 in itertools.pairwise(polygon)]
+    # Include first item at the end for the last combination
+    inner_edge_pairs = itertools.pairwise(dists_to_point + [dists_to_point[0]])
+    # inner_triangles = [pair + (edge_length,) for pair in inner_edge_pairs]
+    inner_triangles = [pair + (edge_length,) for pair in inner_edge_pairs for edge_length in edge_lengths]
 
-def is_contained(polygon:tuple, point:tuple) -> bool:
-    '''
-    This is believed to be true for equilateral triangles but isn't known
-    to work for other kinds of triangles.
-    '''
-    dists = [euc_dist(vertex, point) for vertex in polygon]
-    d = 2 * euc_dist(polygon[0], polygon[1])
-    return sum(dists) <= d
+    sum_of_angles = sum([gamma(p[0], p[1], p[2]) for p in inner_triangles])
 
-def polygon_from_center(center:tuple, radius:tuple) -> tuple:
+    # if sum_of_angles == 360:
+    #     return True
+    # else:
+    #     return False
+    # return inner_triangles
+    return sum_of_angles
+
+def polygon_from_center(center:tuple, radius:int, num_vertices:int) -> tuple:
     '''
-    NOTE: currently defines a 3-polygon (triangle). Will generalize in time.
+    Assume polygon is equilateral.
 
-    Assume triangle is equilateral.
-
-    Given a center and a radius, and assuming the baseline of the triangle is
+    Given a center and a radius, and assuming the baseline of the polygon is
     parallel to the x-axis, we can easily compute the "top" point of the
-    triangle. This is simply (cx, cy+r), where cx and cy are the coordinates
+    polygon. This is simply (cx, cy+r), where cx and cy are the coordinates
     of the center point. (Effectively we're just drawing a straight line of
     length r up from the center point.)
 
-    To compute the other two points of the triangle, we can use trigonometry
-    to compute the center point of the baseline of the triangle, then compute
+    To compute the other two points of the polygon, we can use trigonometry
+    to compute the center point of the baseline of the polygon, then compute
     the length of the baseline, and finally determine points out to the left
     and right from that center baseline point by respectively adding and
     subtracting half the length of the baseline tothe x-coord of the center
     baseline point.
 
-    Returns triangle coordinates := (a, b, c), where a is top center point
-    and b & c are other points moving clockwise.
+    Returns polygon coordinates, wherein the first point is 0 degrees relative
+    to the center, the second 360/N degrees from center, and so on.
     '''
 
     # Alias math.radians for using trig functions, which assume values are
@@ -74,16 +79,22 @@ def polygon_from_center(center:tuple, radius:tuple) -> tuple:
     # Calculated based on fact that the right triangle formed by the center
     # of triangle, the center of the baseline, and either of the other
     # two points is a 30-60-90 triangle.
-    center_to_baseline = radius * math.sin(rad(30))
-    baseline_center_point = center[0], center[1] - center_to_baseline
+    # center_to_baseline = radius * math.sin(rad(30))
+    # baseline_center_point = center[0], center[1] - center_to_baseline
 
-    len_half_baseline = center_to_baseline / math.tan(rad(30))
+    # len_half_baseline = center_to_baseline / math.tan(rad(30))
 
-    a = center[0], center[1] + radius
-    b = baseline_center_point[0] + len_half_baseline, baseline_center_point[1]
-    c = baseline_center_point[0] - len_half_baseline, baseline_center_point[1]
+    # a = center[0], center[1] + radius
+    # b = baseline_center_point[0] + len_half_baseline, baseline_center_point[1]
+    # c = baseline_center_point[0] - len_half_baseline, baseline_center_point[1]
 
-    return a, b, c
+    # return a, b, c
+    alpha = 360 / num_vertices
+
+    x_part = lambda i: radius * math.cos(rad(i * alpha))
+    y_part = lambda i: radius * math.sin(rad(i * alpha))
+
+    return [(round_dec(center[0] + x_part(i)), round_dec(center[1] + y_part(i))) for i in range(num_vertices)]
 
 class FractalGUI(tk.Frame):
     def __init__(self, master=None):
@@ -102,9 +113,10 @@ class FractalGUI(tk.Frame):
         return img
 
 class ChaosGameFractal:
-    def __init__(self, center:tuple, radius:float): # Will add more params in time
+    def __init__(self, center:tuple, radius:float, num_vertices:int): # Will add more params in time
         self.center, self.radius = center, radius
-        self.vertices = polygon_from_center(center, radius)
+        self.vertices = polygon_from_center(center, radius, num_vertices)
+        print(self.vertices)
         self.fractal_pts = {}
 
         ## Setting initial random point
@@ -122,6 +134,7 @@ class ChaosGameFractal:
     def generate_point(self, i:int, jump_factor:float, skip_n:int=0) -> tuple:
         rand_vertex = random.choice(self.vertices)
         new_point = inter_point(rand_vertex, self.fractal_pts[i-1], jump_factor=jump_factor)
+        print(f'Next point generated: {new_point}')
         self.fractal_pts[i] = new_point
         return new_point
 
@@ -130,7 +143,7 @@ if __name__ == '__main__':
     gui = FractalGUI(master=root)
     canvas_image = gui.create_image(900,900)
 
-    game = ChaosGameFractal(center=(450, 450), radius=200)
+    game = ChaosGameFractal(center=(450, 450), radius=400, num_vertices=6)
     
     i = 1
     while True:
@@ -139,3 +152,4 @@ if __name__ == '__main__':
         canvas_image.put('white', point)
         root.update()
         i += 1
+    # canvas_image.put('white', )
